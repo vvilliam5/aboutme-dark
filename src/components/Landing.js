880947739638db3fb1729d8c81784ca09ecfec13
Williams Oluwafemi <realwilliams01@gmail.com>
2020-11-18 20:44:50 +0100

    site update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,11 +6,11 @@ class Landing extends React.Component {
         return (
             <div className="segment landing-container">
                 <h1>Hi There!</h1>
-                <h6>I'm Williams, a Software Engineer and a huge liverpool fan. #YNWA</h6>
-                <h6>With over <span className="emphasis">6 years</span> of experience in Software Engineering, I have had the opportunity to work across all phases of the Software Development
-                    Lifecycle.</h6>
-                <h6><span className="emphasis">Design - Frontend - Backend - DevOps</span></h6>
+                <h6>I'm Williams, capacity of a Full Stack Engineer, preference for Frontend ( i love building aesthetic layouts )</h6>
+                <h6>I've dedicated <span className="emphasis">6 years </span>of my life to mastering the art of Web Engineering, from prototyping and design to pixel perfect User Interfaces and intuitive, satisfying User Interfaces, and i loved every minute of it! I strongly believe theres something new to learn everyday, and i always reach out for that extra piece of knowledge</h6>
+                <h6>Design - <span className="emphasis">Frontend</span> - Backend - DevOps</h6>
                 <div className="segment">
+                    <h5>Links to my Behance, GitHub, linkedin and Blog</h5>
                     <a href="https://www.behance.net/realwilliams" target="_blank" style={{ textDecoration: 'none' }}><button class="unit button" type="button"><i class="fa fa-behance fa-lg" aria-hidden="true"></i></button></a>
                     <a href="https://www.github.com/vvilliam5" target="_blank" style={{ textDecoration: 'none' }}><button class="unit button" type="button"><i class="fa fa-github" aria-hidden="true"></i></button></a>
                     <a href="https://www.linkedin.com/in/vvilliam5" target="_blank" style={{ textDecoration: 'none' }}><button class="unit button" type="button"><i class="fa fa-linkedin" aria-hidden="true"></i></button></a>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import '../css/main.css'
-import mojecmeters from '../img/projects/mojecmeters.png';
-import csms from '../img/projects/csms.png';
-import stockings from '../img/projects/stockings.png';
+import linkedin from '../img/projects/linkedin.png'
 
 class Resume extends React.Component {
     render() {
         return (
             <div className="segment">
-                <h1>Some Recent Projects</h1>
-                <div class="segment">
-                    <a href="https://mojecmeters.com" target="_blank"><button class="unit3 button" type="button"><img src={mojecmeters} class="skillset-img" /></button></a>
-                    <a href="https://ohos-csms.herokuapp.com/" target="_blank"><button class="unit3 button" type="button"><img src={csms} class="skillset-img" /></button></a>
-                    <a href="https://vvilliam5.github.io/stockings/" target="_blank"><button class="unit3 button" type="button"><img src={stockings} class="skillset-img" /></button></a>
-                </div>
-                <div className="segment">
-                    <a href="https://github.com/vvilliam5" style={{ textDecoration: 'none' }} target="_blank"><button class="portfolio button" type="button"><i class="icon ion-md-lock"></i> I want to see more</button></a>
+                <h1>Here's what i've been up to lately</h1>
+                <div className='segment'>
+                    <a href="https://www.linkedin.com/in/vvilliam5/detail/recent-activity/shares/" target="_blank"><button class="unit3" type="button"><img src={linkedin} class="skillset-img" /></button></a>
                 </div>
             </div>
         )
